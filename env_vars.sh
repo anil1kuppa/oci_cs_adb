ff8780b9257fcdd07b531a7deb2863d9a81e1597
anil1kuppa <anil.kuppa@gmail.com>
2022-01-11 20:11:48 +0530

    Update env_vars.sh

--- env_vars.sh
+++ env_vars.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-export comp_name=                                       # The name of the comapartment where ADB is going to live
-export db_name=                                         # Name of the database
-echo "DB Password :" ; read -s db_pwd; export db_pwd    # Read the password for the ADB.                                                     
+ocid_comp=$(oci iam availability-domain list --all | jq -r '.data[0]."compartment-id"')   ##Default compartmentId
+echo "DB Password :" ; read -s db_pwd; export db_pwd    # Read the password for the ADB.                                                         
 export display_name=${db_name}                          # The display name of the ADB instance
 export wallet_file=${db_name}                           # The filename of the Wallet
 export wallet_pwd=${db_pwd}                             # The password for the Wallet
@@ -12,7 +11,6 @@ export conn_string=${db_name}_LOW                       # Connection string for 
 # NOTE: Password must be 12 to 30 characters and contain at least one uppercase letter, one lowercase letter, 
 # and one number. The password cannot contain the double quote (") character or the username "admin".
 
-echo "Compartment Name :" ${comp_name}
 echo "Database Name    :" ${db_name}
 echo "Database Password:" ${db_pwd}
 echo "TNS Conn String  :" ${conn_string}
